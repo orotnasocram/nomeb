29c1494fe6853ea23243f3f2220b3764d32167a3
m4rc0s4ntoro <orotnasocram@github.com>
2026-01-16 14:03:47 +0100

    feat: add initial HTML structure and assets for the application
    - Created index.html with basic structure and linked assets
    - Added vite.svg as a favicon
    - Removed old content from index.html and App.tsx, replacing it with new structure
    - Updated package.json script from "dev" to "npm"
    - Added a new test HTML file with similar structure for testing purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      </div> */}
+      <h1>nomep</h1>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
+      </div> */}
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p> */}
+      {/* <p>paragrafo normale</p>
+      <h2>NomeB</h2> */}
+        <img src="media/test.jpg" alt="Descrizione immagine"></img>
+      <h2>Musica</h2>
+      <p>
+        <audio id="test-audio" controls preload="none">
+          <source src="media/testsound.mp3" type="audio/mpeg"></source>
+          Your browser does not support the audio element.
+        </audio>
       </p>
+      <h2>Live</h2>
+      <p>
+        <ul>
+            <li>2026-01-01 - Live @ locale</li>
+            <li>2026-01-02 - Live @ locale</li>
+            <li>2026-01-03 - Live @ locale</li>
+            <li>2026-01-04 - Live @ locale</li>
+            <li>2026-01-05 - Live @ locale</li>
+            <li>2026-01-06 - Live @ locale</li>
+            <li>2026-01-07 - Live @ locale</li>
+        </ul>
+      </p>
+      
     </>
   )
 }
